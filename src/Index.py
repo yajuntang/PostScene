a81b6f114bf52a630f3c9792f985617259c377f0
Cherly Tong <>
2020-04-20 11:03:48 +0800

    index

--- src/Index.py
+++ src/Index.py
@@ -1,9 +1,11 @@
 from post_scene.post_scene import PostScene
 
-xmind_path = './yaml/demo.yaml'  # 脚本文件的路径
+yaml_path = './yaml/demo.yaml'  # 脚本文件的路径
+xmind_path = './xmind/demo.xmind'  # 脚本文件的路径
 api_document_path = './api_document/demo.postman_collection.json'  # postman json data 文件的路径
 
 
-PostScene.covert(xmind_path, api_document_path,scene_dirs='./scene')
+# PostScene.covert(xmind_path, api_document_path,scene_dirs='./scene')
+PostScene.covert(yaml_path, api_document_path,scene_dirs='./scene')
 
 
