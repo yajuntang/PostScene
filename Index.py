65db787c14d204ab878cbcc776033471ef7359d8
Cherly Tong <>
2020-04-20 11:01:42 +0800

    arrange

--- Index.py
+++
@@ -1,9 +0,0 @@
-from post_scene.post_scene import PostScene
-
-xmind_path = '../xmind/demo.yaml'  # 脚本文件的路径
-api_document_path = '../api_document/demo.postman_collection.json'  # postman json data 文件的路径
-
-
-PostScene.covert(xmind_path, api_document_path)
-
-
